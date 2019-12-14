11eac84bf695573a26a2dc5d20437bb2614ae6c5
eirikfoss <eirikfoss@gmail.com>
2019-12-14 12:38:17 +0100

    git ignore

--- src/components/scoreboard/Scoreboard.js
+++ src/components/scoreboard/Scoreboard.js
@@ -106,40 +106,6 @@ const Scoreboard = () => {
     );
   };
 
-  /*
-calculateRatingAdjustment = matchData => {
-    //const blueRating = 1000;
-    //const redRating = 1000;
-    const blueRating = this.getTeamRating(matchData.teams.blue);
-    const redRating = this.getTeamRating(matchData.teams.red);
-    const scoreBlue = matchData.teams.blue.score;
-    const scoreRed = matchData.teams.red.score;
-    const ratingFactor = 1000;
-    const expectedBlue =
-      1 / (1 + Math.pow(10, (redRating - blueRating) / ratingFactor));
-    const expectedRed =
-      1 / (1 + Math.pow(10, (blueRating - redRating) / ratingFactor));
-    console.log("Expected blue: " + expectedBlue);
-    const newRatingBlue =
-      scoreBlue > scoreRed
-        ? Math.round(blueRating + 100 * (10 / (10 + scoreRed) - expectedBlue))
-        : Math.round(
-            blueRating + 100 * (1 - 10 / (10 + scoreBlue) - expectedBlue)
-          );
-    const newRatingRed =
-      scoreRed > scoreBlue
-        ? Math.round(redRating + 100 * (10 / (10 + scoreBlue) - expectedRed))
-        : Math.round(
-            redRating + 100 * (1 - 10 / (10 + scoreRed) - expectedRed)
-          );
-    const adjustmentBlue = blueRating - newRatingBlue;
-    const adjustmentRed = redRating - newRatingRed;
-    const ratingAdjustments = { blue: adjustmentBlue, red: adjustmentRed };
-    return ratingAdjustments;
-  };
-  
-  */
-
   /* 
  const renderNewMatchOptions = (matchOver, toggleMatchActive) => {
     if (matchOver) {
